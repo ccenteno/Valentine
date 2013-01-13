92afa75401deb2926221b8537e215d5461b754c9
Cesar Augusto <ccentenor@gmail.com>
2013-01-12 23:34:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valentine::Application.config.secret_token = '0ca143e8b599599ad4e5e84b723e0eec0c1663e1d86632c7d4ae49a59513ede5f7062c9ef73eb1de5abf15403212a97d98c9425ba4d99ea13ebe1d5a6ec0ce25'
